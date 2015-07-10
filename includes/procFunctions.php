c332a4d5374ce521250bd084a2005ae49d336161
ghidalgor <ghidalgor@gmail.com>
2015-07-09 23:04:22 -0600

    Second part. It include function coloBar

--- includes/procFunctions.php
+++ includes/procFunctions.php
@@ -11,4 +11,31 @@ Twitter: @websoundcr
 GitHub:  @ghidalgor
 Please refer me :-);
 */
+
+function coloBar($percentage, $cssRule)
+{
+	$htmlUnidadHeader = "<div class=\"coloBar border-all-round\">";
+	$htmlEndUnit = "</div>";
+
+	#Save html code
+	$html = ""; #It's all HTML code
+	$htmlUnit = ""; #HTML code of the Bar. The WIDTH depend of $percentage
+
+	if(is_numeric($percentage) && $percentage>=0 && $percentage<=100)
+	{
+		#Create a DIV thet represents the "color bar".
+		$htmlUnit = $htmlUnit."<div style=\"width:".$percentage."%;\" class=\"".$cssRule."\">".$percentage."%&nbsp;</div>";
+
+		$htmlUnit = $htmlUnit."<div class=\"clear\"></div>"; #Clear both
+
+		#Complete HTML / All Structure
+		$html = $htmlUnidadHeader.$htmlUnit.$htmlEndUnit;
+	}
+	else
+	{
+		$html = $htmlUnidadHeader."&nbsp;".$htmlEndUnit; #Empty, not color bar
+	}
+
+	echo $html;
+}
 ?>
